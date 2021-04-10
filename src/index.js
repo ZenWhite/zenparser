28a70dbbdaea994be4f3b8c8faa6842e82e9a9c1
Andrew Letyuchiy <misterbook13@mail.ru>
2021-04-10 10:23:05 +0300

    init puppeteer

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import cheerio from 'cheerio'
 import chalk from 'chalk'
 
-const URL = 'https://lcsc.com/products/Cables-Wires_328.html'
+import { getPageContent } from './utils/getPageContent.js'
+
+const URL = 'https://lcsc.com/products/Cables-Wires_328.html';
+
+(async function main() {
+    try {
+        const pageContent = await getPageContent(URL)
+        console.log( chalk.green(pageContent) )
+    } catch(err) {
+        console.log( chalk.red('Что-то пошло не так!\n') )
+        console.log(err)
+    }
+})()
